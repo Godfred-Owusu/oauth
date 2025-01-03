fd99f4145e31eae8391f234c9ec57e4297eaea53
godfr <gowusu018@gmail.com>
2025-01-03 20:46:45 +0100

    add the Ui for google oauth testing

---
+++ frontend/Google-Oauth/src/App.jsx
@@ -0,0 +1,35 @@
+import "./App.css";
+
+const navigate = (url) => {
+  window.location.href = url;
+};
+
+async function auth() {
+  try {
+    const response = await fetch("http://127.0.0.1:3000/oauth/request", {
+      method: "POST",
+    });
+    const data = await response.json();
+    if (data.url) {
+      navigate(data.url);
+    } else {
+      console.error("Failed to get OAuth URL");
+    }
+  } catch (error) {
+    console.error("Error during authentication:", error.message);
+  }
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to my site</h1>
+      <h3>Google OAuth</h3>
+      <button type="button" onClick={auth}>
+        Sign in with Google
+      </button>
+    </div>
+  );
+}
+
+export default App;
